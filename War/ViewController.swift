71b04693b7cc05ce83e01880de348161a27d426f
Mustafa Shikora <mshik3@gmail.com>
2016-12-21 00:17:02 -0600

    First iPhone app

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
 
+    @IBOutlet weak var PlayerLabel: UILabel!
+    var PlayerScore = 0
+    @IBOutlet weak var CPULabel: UILabel!
+    var CPUScore = 0
+    
+    var cards: Array<UIImage> = [UIImage(named: "card2")!, UIImage(named: "card3")!, UIImage(named: "card4")!, UIImage(named: "card5")!, UIImage(named: "card6")!, UIImage(named: "card7")!, UIImage(named: "card8")!, UIImage(named: "card9")!, UIImage(named: "card10")!, UIImage(named: "jack")!, UIImage(named: "queen")!, UIImage(named: "king")!, UIImage(named: "ace")!]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func dealTapped(_ sender: Any) {
+        let leftRand = Int(arc4random_uniform(13))
+        let rightRand = Int(arc4random_uniform(13))
+        
+        leftImageView.image = cards[leftRand]
+        rightImageView.image = cards[rightRand]
+        
+        if leftRand > rightRand{
+            //left wins
+            PlayerScore+=1
+            PlayerLabel.text = String(PlayerScore)
+        }
+        else if leftRand==rightRand{
+            //do nothing
+        }
+        else{
+            //right wins
+            CPUScore+=1
+            CPULabel.text = String(CPUScore)
+        }
+    }
 
 }
 
